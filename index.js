126adb708f218f6a9367206c17f4b1857914ada5
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-01-15 14:40:42 +0530

    add database connection

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const connectDB = require('./config/db')
+
+// db connection
+connectDB();
+
 
 // Body parser
 app.use(express.json());
@@ -21,3 +26,4 @@ app.use(cors());
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`)
 })
+
