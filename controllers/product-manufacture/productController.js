07275d099232b657cabfd4629a14f91d2cadd75e
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-02-10 21:40:39 +0530

    change cors error

--- controllers/product-manufacture/productController.js
+++ controllers/product-manufacture/productController.js
@@ -1,99 +1,108 @@
-const asyncHandler = require('../../middleware/async')
-const ErrorResponse = require('../../utils/errorResponse');
-const Product = require('../../models/Product')
-
+const asyncHandler = require("../../middleware/async");
+const ErrorResponse = require("../../utils/errorResponse");
+const Product = require("../../models/Product");
 
 // @desc     Add Products in Product Manufacture
 // @route    POST /api/v1/product
 // @access   Private
 
 const addProduct = asyncHandler(async (req, res, next) => {
-    const { image, productName, productPrice, description, productIncludes } = req.body;
-  
-    const product = await Product.create({
-      image,
-      productName,
-      productPrice,
-      description,
-      productIncludes,
-      owner: req.user._id
-    });
+  const { image, productName, productPrice, description, productIncludes } =
+    req.body;
 
-    res.status(201).json({success: true, message: "Product creating success"})
-  
+  const product = await Product.create({
+    image,
+    productName,
+    productPrice,
+    description,
+    productIncludes,
+    owner: req.user._id,
   });
 
+  res.status(201).json({ success: true, message: "Product creating success" });
+});
 
 // @desc     Get All Products
 // @route    GET /api/v1/products
 // @access   Public
 
-const getAllProducts = asyncHandler(async(req, res, next) => {
-    res.status(200).json(res.advancedResults);
-})
+const getAllProducts = asyncHandler(async (req, res, next) => {
+  res.status(200).json(res.advancedResults);
+});
 
 // @desc     Get Single Products
 // @route    GET /api/v1/products/:id
 // @access   Public
 
-const getProduct = asyncHandler(async(req, res, next) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
+const getProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
 
-    if(!product) {
-        return next(new ErrorResponse("Product Not Found", 404))
-    }
-    
-    res.status(200).json({success: true, data: product});
-})
+  if (!product) {
+    return next(new ErrorResponse("Product Not Found", 404));
+  }
+
+  res.status(200).json({ success: true, data: product });
+});
 
 // @desc     Update Product
 // @route    PUT /api/v1/products/:id
 // @access   Private
 
-const updateProduct = asyncHandler(async(req, res, next) => {
-    const { id } = req.params;
-    const { image , productName, productPrice, description, productIncludes } = req.body
-    const product = await Product.findById(id);
+const updateProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const { image, productName, productPrice, description, productIncludes } =
+    req.body;
+  const product = await Product.findById(id);
 
-    if(!product) {
-        return next(new ErrorResponse("Product Not Found", 404))
-    }
+  if (!product) {
+    return next(new ErrorResponse("Product Not Found", 404));
+  }
 
-    if(product.owner.toString() !== req.user._id.toString()) {
-        return next(new ErrorResponse(`You have no permission to update this product`,403))
-    }
+  if (product.owner.toString() !== req.user._id.toString()) {
+    return next(
+      new ErrorResponse(`You have no permission to update this product`, 403)
+    );
+  }
 
-    product.image = image || product.image;
-    product.productName = productName || product.productName;
-    product.productPrice = productPrice || product.productPrice;
-    product.description = description || product.description;
-    product.productIncludes = productIncludes || product.productIncludes
+  product.image = image || product.image;
+  product.productName = productName || product.productName;
+  product.productPrice = productPrice || product.productPrice;
+  product.description = description || product.description;
+  product.productIncludes = productIncludes || product.productIncludes;
 
-    await product.save();
-    
-    res.status(200).json({success: true, message: "Product Update success"});
-})
+  await product.save();
+
+  res.status(200).json({ success: true, message: "Product Update success" });
+});
 
 // @desc     Delete Product
 // @route    DELETE /api/v1/products/:id
 // @access   Private
 
-const deleteProduct = asyncHandler(async(req, res, next) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
+const deleteProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
 
-    if(!product) {
-        return next(new ErrorResponse("Product Not Found", 404))
-    }
+  if (!product) {
+    return next(new ErrorResponse("Product Not Found", 404));
+  }
 
-    if(product.owner.toString() !== req.user._id.toString()) {
-        return next(new ErrorResponse(`You have no permission to delete this product`,403))
-    }
+  if (product.owner.toString() !== req.user._id.toString()) {
+    return next(
+      new ErrorResponse(`You have no permission to delete this product`, 403)
+    );
+  }
 
-    await product.deleteOne();
-    
-    res.status(200).json({success: true, message: "Product Delete success"});
-})
+  await product.deleteOne();
 
-module.exports = { addProduct, getAllProducts, getProduct, updateProduct, deleteProduct };
+  res.status(200).json({ success: true, message: "Product Delete success" });
+});
+
+module.exports = {
+  addProduct,
+  getAllProducts,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+};
--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const cors = require('cors')
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const connectDB = require('./config/db')
-const errorHandler = require("./middleware/error")
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
-const auth = require('./routes/authRoute');
-const product = require('./routes/product-manufacture/productRoutes');
-const shop = require('./routes/resturant-manufacture/shopRoutes');
-const food = require('./routes/resturant-manufacture/foodRoutes');
-const farmerProduct = require('./routes/farmer/productRoutes');
-const user = require('./routes/users/userRoutes');
+const auth = require("./routes/authRoute");
+const product = require("./routes/product-manufacture/productRoutes");
+const shop = require("./routes/resturant-manufacture/shopRoutes");
+const food = require("./routes/resturant-manufacture/foodRoutes");
+const farmerProduct = require("./routes/farmer/productRoutes");
+const user = require("./routes/users/userRoutes");
 
 // db connection
 connectDB();
-
 
 // Body parser
 app.use(express.json());
@@ -29,26 +28,25 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Enable CORS
-app.use(cors({
-    origin: 'http://localhost:5173', // Allow frontend origin
-    credentials: true, // Allow cookies to be sent
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-}));
-
+app.use(
+  cors({
+    origin: true, // Reflects the request's origin
+    credentials: true, // Allows cookies and authentication headers
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  })
+);
 
 // register all routes
-app.use('/api/auth' , auth);
-app.use('/api/product-manufacture/products', product);
-app.use('/api/resturant-manufacture/shops', shop);
-app.use('/api/resturant-manufacture/foods', food);
-app.use('/api/farmer/products', farmerProduct);
-app.use('/api/users/buyProducts', user);
+app.use("/api/auth", auth);
+app.use("/api/product-manufacture/products", product);
+app.use("/api/resturant-manufacture/shops", shop);
+app.use("/api/resturant-manufacture/foods", food);
+app.use("/api/farmer/products", farmerProduct);
+app.use("/api/users/buyProducts", user);
 
 // use the error handling middleware
 app.use(errorHandler);
 
-
 app.listen(PORT, () => {
-    console.log(`Server running on PORT: ${PORT}`)
-})
-
+  console.log(`Server running on PORT: ${PORT}`);
+});
