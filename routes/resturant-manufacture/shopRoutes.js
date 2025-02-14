9fa729df961f623be47f0721066b8d2b63fe9710
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-02-14 23:05:04 +0530

    some changes

--- routes/resturant-manufacture/shopRoutes.js
+++ routes/resturant-manufacture/shopRoutes.js
@@ -1,14 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const advancedResults = require('../../middleware/advancedResults');
-const { addShop, getAllShops, getShop, updateShop, deleteShop } = require('../../controllers/resturant-manufacture/shopController');
-const { protect, authorize } = require('../../middleware/authMiddleware');
-const Shop = require('../../models/Shop');
+const advancedResults = require("../../middleware/advancedResults");
+const {
+  addShop,
+  getAllShops,
+  getShop,
+  updateShop,
+  deleteShop,
+} = require("../../controllers/resturant-manufacture/shopController");
+const { protect, authorize } = require("../../middleware/authMiddleware");
+const Shop = require("../../models/Shop");
 
-router.route('/')
-    .post(protect, authorize("resturantManufacture"), addShop)
-    .get(advancedResults(Shop, "foods"), getAllShops);
-router.route('/:id').get(getShop).put(protect, authorize("resturantManufacture"), updateShop).delete(protect, authorize("resturantManufacture"), deleteShop);
+router
+  .route("/")
+  .post(protect, authorize("resturantManufacture"), addShop)
+  .get(advancedResults(Shop, "foods"), getAllShops);
+router
+  .route("/:id")
+  .get(getShop)
+  .put(protect, authorize("resturantManufacture"), updateShop)
+  .delete(protect, authorize("resturantManufacture"), deleteShop);
 
 module.exports = router;
