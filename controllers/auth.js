a449f675b9b05a671cc032a20d5321bce2c104e8
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-02-15 14:05:25 +0530

    change some backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,35 @@
-const Users = require('../models/Users');
-const asyncHandler = require('../middleware/async')
-const ErrorResponse = require('../utils/errorResponse');
-
+const Users = require("../models/Users");
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
 
 // @desc     Register users
 // @route    POST /api/v1/auth/register
 // @access   Public
 
 const registerUsers = asyncHandler(async (req, res, next) => {
-    const { firstName, lastName, nic, contactNumber, email, role, password, confiremPassword } = req.body;
-  
-    const user = await Users.create({
-      firstName,
-      lastName,
-      nic,
-      contactNumber,
-      email,
-      role,
-      password,
-    });
-  
-    sendTokenResponse(user, 200, res);
+  const {
+    firstName,
+    lastName,
+    nic,
+    contactNumber,
+    email,
+    role,
+    password,
+    confiremPassword,
+  } = req.body;
+
+  const user = await Users.create({
+    firstName,
+    lastName,
+    nic,
+    contactNumber,
+    email,
+    role,
+    password,
   });
 
+  sendTokenResponse(user, 200, res);
+});
 
 // @desc     Login user
 // @route    POST /api/v1/auth/login
@@ -33,37 +40,36 @@ const login = asyncHandler(async (req, res, next) => {
 
   // Validate email and password
   if (!email || !password) {
-    return next(new ErrorResponse('Please provide an email and password', 400));
+    return next(new ErrorResponse("Please provide an email and password", 400));
   }
 
   // Check for user
-  const user = await Users.findOne({ email }).select('+password');
+  const user = await Users.findOne({ email }).select("+password");
 
-   if (!user) {
-    return next(new ErrorResponse('Invalid credentials', 401));
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
   }
 
-  if(role !== user.role) {
-    return next(new ErrorResponse('Access denied', 403))
+  if (role !== user.role) {
+    return next(new ErrorResponse("Access denied", 403));
   }
 
   // Check if password matches
   const isMatch = await user.matchPassword(password);
 
   if (!isMatch) {
-    return next(new ErrorResponse('Invalid credentials', 401));
+    return next(new ErrorResponse("Invalid credentials", 401));
   }
 
   sendTokenResponse(user, 200, res);
 });
-
 
 // @desc     Logout user
 // @route    GET /api/v1/auth/logout
 // @access   Private
 
 const logout = asyncHandler(async (req, res, next) => {
-  res.cookie('token', 'none', {
+  res.cookie("token", "none", {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
   });
@@ -74,36 +80,34 @@ const logout = asyncHandler(async (req, res, next) => {
   });
 });
 
-
-
-
-  // Get token from model, create cookie and send response
+// Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
-    // Create token
-    const token = user.getSignedJwtToken();
-  
-    const option = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-    };
-  
-    if (process.env.NODE_ENV === 'production') {
-      option.secure = true;
-    }
-  
-    res.status(statusCode).cookie('token', token, option).json({
-      success: true,
-      user: {
-        userName: user.firstName + " " + user.lastName,
-        userEmail: user.email,
-        userRole: user.role
-      }
-    });
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  const option = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
   };
 
+  if (process.env.NODE_ENV === "production") {
+    option.secure = true;
+  }
 
+  res
+    .status(statusCode)
+    .cookie("token", token, option)
+    .json({
+      success: true,
+      user: {
+        userId: user._id,
+        userName: user.firstName + " " + user.lastName,
+        userEmail: user.email,
+        userRole: user.role,
+      },
+    });
+};
 
-
-  module.exports = {registerUsers , login, logout};
+module.exports = { registerUsers, login, logout };
--- controllers/resturant-manufacture/foodController.js
+++ controllers/resturant-manufacture/foodController.js
@@ -1,106 +1,115 @@
-const asyncHandler = require('../../middleware/async')
-const ErrorResponse = require('../../utils/errorResponse');
-const Food = require('../../models/Food');
-const Shop = require('../../models/Shop');
-
+const asyncHandler = require("../../middleware/async");
+const ErrorResponse = require("../../utils/errorResponse");
+const Food = require("../../models/Food");
+const Shop = require("../../models/Shop");
 
 // @desc     Add Food in Resturant Manufacture
 // @route    POST /api/v1/food
 // @access   Private
 
 const addFood = asyncHandler(async (req, res, next) => {
-    const { image, productName, productPrice, description, productIncludes, shop } = req.body;
+  const {
+    image,
+    productName,
+    productPrice,
+    description,
+    productIncludes,
+    shop,
+  } = req.body;
 
-    const isShop = await Shop.findById(shop);
-    if(!isShop) {
-        return next(new ErrorResponse("Shop is not available for add foods", 404))
-    }
-  
-    const food = await Food.create({
-      image,
-      productName,
-      productPrice,
-      description,
-      productIncludes,
-      shop,
-      owner: req.user._id
-    });
+  const isShop = await Shop.findById(shop);
+  if (!isShop) {
+    return next(new ErrorResponse("Shop is not available for add foods", 404));
+  }
 
-    res.status(201).json({success: true, message: "Food creating success"})
-  
+  const food = await Food.create({
+    image,
+    productName,
+    productPrice,
+    description,
+    productIncludes,
+    shop,
+    owner: req.user._id,
   });
 
+  res.status(201).json({ success: true, message: "Food creating success" });
+});
 
 // @desc     Get All Foods
 // @route    GET /api/v1/foods
 // @access   Public
 
-const getAllFoods = asyncHandler(async(req, res, next) => {
-    res.status(200).json(res.advancedResults);
-})
+const getAllFoods = asyncHandler(async (req, res, next) => {
+  res.status(200).json(res.advancedResults);
+});
 
 // @desc     Get Single Food
 // @route    GET /api/v1/foods/:id
 // @access   Public
 
-const getFood = asyncHandler(async(req, res, next) => {
-    const { id } = req.params;
-    const food = await Food.findById(id);
+const getFood = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const food = await Food.findById(id);
 
-    if(!food) {
-        return next(new ErrorResponse("Food Not Found", 404))
-    }
-    
-    res.status(200).json({success: true, data: food});
-})
+  if (!food) {
+    return next(new ErrorResponse("Food Not Found", 404));
+  }
+
+  res.status(200).json({ success: true, data: food });
+});
 
 // @desc     Update Food
 // @route    PUT /api/v1/foods/:id
 // @access   Private
 
-const updateFood = asyncHandler(async(req, res, next) => {
-    const { id } = req.params;
-    const { image , productName, productPrice, description, productIncludes } = req.body
-    const food = await Food.findById(id);
+const updateFood = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const { image, productName, productPrice, description, productIncludes } =
+    req.body;
+  const food = await Food.findById(id);
 
-    if(!food) {
-        return next(new ErrorResponse("Food Not Found", 404))
-    }
+  if (!food) {
+    return next(new ErrorResponse("Food Not Found", 404));
+  }
 
-    if(food.owner.toString() !== req.user._id.toString()) {
-        return next(new ErrorResponse(`You have no permission to update this product`,403))
-    }
+  if (food.owner.toString() !== req.user._id.toString()) {
+    return next(
+      new ErrorResponse(`You have no permission to update this product`, 403)
+    );
+  }
 
-    food.image = image || food.image;
-    food.productName = productName || food.productName;
-    food.productPrice = productPrice || food.productPrice;
-    food.description = description || food.description;
-    food.productIncludes = productIncludes || food.productIncludes
+  food.image = image || food.image;
+  food.productName = productName || food.productName;
+  food.productPrice = productPrice || food.productPrice;
+  food.description = description || food.description;
+  food.productIncludes = productIncludes || food.productIncludes;
 
-    await food.save();
-    
-    res.status(200).json({success: true, message: "Food Update success"});
-})
+  await food.save();
+
+  res.status(200).json({ success: true, message: "Food Update success" });
+});
 
 // @desc     Delete Food
 // @route    DELETE /api/v1/foods/:id
 // @access   Private
 
-const deleteFood = asyncHandler(async(req, res, next) => {
-    const { id } = req.params;
-    const food = await Food.findById(id);
+const deleteFood = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const food = await Food.findById(id);
 
-    if(!food) {
-        return next(new ErrorResponse("Food Not Found", 404))
-    }
+  if (!food) {
+    return next(new ErrorResponse("Food Not Found", 404));
+  }
 
-    if(food.owner.toString() !== req.user._id.toString()) {
-        return next(new ErrorResponse(`You have no permission to delete this product`,403))
-    }
+  if (food.owner.toString() !== req.user._id.toString()) {
+    return next(
+      new ErrorResponse(`You have no permission to delete this product`, 403)
+    );
+  }
 
-    await food.deleteOne();
-    
-    res.status(200).json({success: true, message: "Food Delete success"});
-})
+  await food.deleteOne();
+
+  res.status(200).json({ success: true, message: "Food Delete success" });
+});
 
 module.exports = { addFood, getAllFoods, getFood, updateFood, deleteFood };
